62d2f4bb3be42066aa4d7fa5febc5063ebbbb2b1
iostsicaro <iostsicaro@gmail.com>
2021-09-02 01:00:43 -0300

    Arquivo de conexão MongoDB

---
+++ src/mongodb.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const conexaoMongoDb = async () => {
+    mongoose.connect(
+        process.env.DATABASE_URL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    );
+
+    const db = mongoose.connection;
+    db.on('error', (error) => console.log(error));
+    db.once('open', () => console.log('Connected to the database!'));
+}
+
+module.exports = conexaoMongoDb;
